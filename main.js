68a71b3c26c04215c85b4a986d2c74307d6a56e4
Nicky Meuleman <nicky.meuleman@gmail.com>
2017-09-13 15:11:19 +0200

    add minimax algorithm

--- main.js
+++ main.js
@@ -30,7 +30,7 @@ function startGame() {
 function turnClick(e) {
     if (typeof origBoard[e.target.id] == "number") {
         turn(e.target.id, huPlayer)
-        if (!checkTie()) {
+        if (!checkWin(origBoard, huPlayer) && !checkTie()) {
             turn(bestSpot(), aiPlayer);
         }
     }
@@ -81,7 +81,7 @@ function emptySquares() {
     return origBoard.filter(elem => typeof elem == "number");
 }
 function bestSpot() {
-    return emptySquares()[0];
+    return minimax(origBoard, aiPlayer).index;
 }
 
 function checkTie() {
@@ -94,4 +94,53 @@ function checkTie() {
         return true;
     }
     return false;
+}
+
+function minimax(newBoard, player) {
+    var availSpots = emptySquares(newBoard);
+
+//PR request beau carner github with player to huplayer
+    if (checkWin(newBoard, huPlayer)) {
+        return {score: -10};
+    } else if (checkWin(newBoard, aiPlayer)) {
+        return {score: 10};
+    } else if (availSpots.length === 0) {
+        return {score: 0};
+    }
+    var moves = [];
+    for (var i = 0; i < availSpots.length; i++) {
+        var move = {};
+        move.index = newBoard[availSpots[i]];
+        newBoard[availSpots[i]] = player;
+
+        if (player == aiPlayer) {
+            var result = minimax(newBoard, huPlayer);
+            move.score = result.score;
+        } else {
+            var result = minimax(newBoard, aiPlayer);
+            move.score = result.score;
+        }
+        //reset newboard to state before
+        newBoard[availSpots[i]] = move.index;
+        moves.push(move);
+    }
+    var bestMove;
+    if (player === aiPlayer) {
+        var bestScore = -10000;
+        for (var i = 0; i < moves.length; i++) {
+            if (moves[i].score > bestScore) {
+                bestScore = moves[i].score;
+                bestMove = i;
+            }
+        }
+    } else {
+        var bestScore = 10000;
+        for (var i = 0; i < moves.length; i++) {
+            if (moves[i].score < bestScore) {
+                bestScore = moves[i].score;
+                bestMove = i;
+            }
+        }
+    }
+    return moves[bestMove];
 }
